cc0d4f7d1148d4e4015d4dc298015d06891c28e4
baby7305 <songhaibei7305@163.com>
2019-09-06 11:26:51 +0800

    Grid Pane微改动

--- src/main/java/com/example/gui/layoutPane/GridPaneDemo.java
+++ src/main/java/com/example/gui/layoutPane/GridPaneDemo.java
@@ -23,23 +23,55 @@ public class GridPaneDemo extends Application {
         gridpane.setVgap(10);
         gridpane.setPadding(new Insets(10, 10, 10, 10));
 
+        gridpane.setGridLinesVisible(true);
+
         final int column = 3;
         final int row = 4;
 
         Button button[][] = new Button[column][row];
 
-        for (int i = 0; i < column; i++) {
-            for (int j = 0; j < row; j++) {
-                button[i][j] = new Button("(" + j + ":" + i + ")");
-                button[i][j].setPrefSize(60, 20);
+        button[0][0] = new Button("(" + 0 + ":" + 0 + ")");
+        button[0][0].setPrefSize(130, 20);
+        // Start at (0, 0) and colspan = 2
+        gridpane.add(button[0][0], 0, 0, 2, 1);
 
-                gridpane.add(button[i][j], i, j);
-            }
-        }
+        button[2][0] = new Button("(" + 0 + ":" + 2 + ")");
+        button[2][0].setPrefSize(60, 20);
+        // Start at (0, 2)
+        gridpane.add(button[2][0], 2, 0);
+
+        button[0][1] = new Button("(" + 1 + ":" + 0 + ")");
+        button[0][1].setPrefSize(60, 20);
+        // Start at (1, 0)
+        gridpane.add(button[0][1], 0, 1);
+
+        button[1][1] = new Button("(" + 1 + ":" + 1 + ")");
+        button[1][1].setPrefSize(130, 20);
+        // Start at (1, 1) and colspan = 2
+        gridpane.add(button[1][1], 1, 1, 2, 1);
+
+        button[0][2] = new Button("(" + 2 + ":" + 0 + ")");
+        button[0][2].setPrefSize(60, 50);
+        // Start at (2, 0) and rowspan = 2
+        gridpane.add(button[0][2], 0, 2, 1, 2);
+
+        button[1][2] = new Button("(" + 2 + ":" + 1 + ")");
+        button[1][2].setPrefSize(60, 20);
+        // Start at (2, 1)
+        gridpane.add(button[1][2], 1, 2);
+
+        button[1][3] = new Button("(" + 3 + ":" + 1 + ")");
+        button[1][3].setPrefSize(60, 20);
+        // Start at (3, 1)
+        gridpane.add(button[1][3], 1, 3);
+
+        button[2][2] = new Button("(" + 2 + ":" + 2 + ")");
+        button[2][2].setPrefSize(60, 50);
+        // Start at (2, 2) and rowspan = 2
+        gridpane.add(button[2][2], 2, 2, 1, 2);
 
         // Set the Layout Pane of Scene
         Scene scene = new Scene(gridpane);
-
         // Set the title of Stage
         primaryStage.setTitle("Grid Pane Demo");
         // Set the width of Stage
