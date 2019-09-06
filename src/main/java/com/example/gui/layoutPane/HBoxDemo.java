b78225ab1f6d4c6a782d7c97ab3a89c02969a454
baby7305 <songhaibei7305@163.com>
2019-09-06 11:29:51 +0800

    HBox

---
+++ src/main/java/com/example/gui/layoutPane/HBoxDemo.java
@@ -0,0 +1,60 @@
+package com.example.gui.layoutPane;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+
+/**
+ * @author Leo Huang
+ */
+public class HBoxDemo extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Label lblURL = new Label("URL:");
+
+        TextField txtURL = new TextField();
+
+        Button btnGo = new Button("Go");
+        btnGo.setPrefSize(60, 20);
+
+        // Define HBox
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(20, 10, 10, 10));
+        hbox.setSpacing(10);
+
+        // Sets the horizontal grow priority to ALWAYS
+        HBox.setHgrow(txtURL, Priority.ALWAYS);
+
+        // Add all components to HBox
+        hbox.getChildren().addAll(lblURL, txtURL, btnGo);
+
+        // Set the Layout Pane of Scene
+        Scene scene = new Scene(hbox);
+        // Set the title of Stage
+        primaryStage.setTitle("HBox Demo");
+        // Set the width of Stage
+        primaryStage.setWidth(300);
+        // Set the height of Stage
+        primaryStage.setHeight(100);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+
+        // Show Stage
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
