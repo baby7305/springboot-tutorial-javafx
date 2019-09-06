2f3f429c19b571597463fd14ff22a9b0a2765828
baby7305 <songhaibei7305@163.com>
2019-09-06 15:08:25 +0800

    Gaussian Blur升级，参数可调

--- src/main/java/com/example/gui/effect/blur/GaussianBlurDemo.java
+++ src/main/java/com/example/gui/effect/blur/GaussianBlurDemo.java
@@ -1,34 +1,47 @@
 package com.example.gui.effect.blur;
 
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.text.DecimalFormat;
+
 /**
  * @author Leo Huang
  */
 public class GaussianBlurDemo extends Application {
+    GaussianBlur gaussianblur;
+
+    Text text;
 
     @Override
     public void start(Stage primaryStage) {
-        GaussianBlur gaussianblur = new GaussianBlur();
+        gaussianblur = new GaussianBlur();
         gaussianblur.setRadius(15.0);
 
-        Text text = new Text();
+        text = new Text();
+        text.setEffect(gaussianblur);
         text.setText("Gaussian Blur");
         text.setFill(Color.BLACK);
         text.setFont(Font.font("Arial Black", FontWeight.BOLD, 60));
 
-        text.setEffect(gaussianblur);
+        GridPane gridpane = createPane();
 
         BorderPane borderpane = new BorderPane();
         borderpane.setCenter(text);
+        borderpane.setBottom(gridpane);
 
         // Set the Layout Pane of Scene
         Scene scene = new Scene(borderpane);
@@ -37,12 +50,48 @@ public class GaussianBlurDemo extends Application {
         // Set the width of Stage
         primaryStage.setWidth(500);
         // Set the height of Stage
-        primaryStage.setHeight(250);
+        primaryStage.setHeight(400);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
 
         // Show Stage
         primaryStage.show();
+    }
+
+    private GridPane createPane() {
+
+        // Set Radius
+        Label title1 = new Label("Radius:");
+
+        final Label lblValue1 = new Label(new DecimalFormat("0.0").format(gaussianblur.getRadius()));
+        lblValue1.setPrefSize(100, 25);
+
+        final Slider slider1 = new Slider();
+        slider1.setOrientation(Orientation.HORIZONTAL);
+        slider1.setMin(0.0);
+        slider1.setMax(63.0);
+        slider1.setValue(gaussianblur.getRadius());
+        slider1.setMinorTickCount(1);
+        slider1.setMajorTickUnit(2.0);
+        slider1.setBlockIncrement(2.0);
+        slider1.setPrefSize(200, 25);
+        slider1.setSnapToTicks(true);
+
+        slider1.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            gaussianblur.setRadius(slider1.getValue());
+            lblValue1.setText(new DecimalFormat("0.0").format(slider1.getValue()));
+        });
+
+        GridPane gridpane = new GridPane();
+        gridpane.setStyle("-fx-background-color: #d4d0c8;");
+        gridpane.setPadding(new Insets(10, 10, 10, 10));
+        gridpane.setHgap(5);
+        gridpane.setVgap(5);
+        gridpane.add(title1, 0, 0);
+        gridpane.add(slider1, 1, 0);
+        gridpane.add(lblValue1, 2, 0);
+
+        return gridpane;
     }
 
     /**
