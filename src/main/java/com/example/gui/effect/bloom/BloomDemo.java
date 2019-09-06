a4ba8f0067fc1093f6d7d1495eedc3dac7fd8588
baby7305 <songhaibei7305@163.com>
2019-09-06 15:35:28 +0800

    绽放升级，参数可调

--- src/main/java/com/example/gui/effect/bloom/BloomDemo.java
+++ src/main/java/com/example/gui/effect/bloom/BloomDemo.java
@@ -1,43 +1,93 @@
 package com.example.gui.effect.bloom;
 
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.effect.Bloom;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.text.DecimalFormat;
 
 /**
  * @author Leo Huang
  */
 public class BloomDemo extends Application {
+    Bloom bloom;
+
+    ImageView imageview;
 
     @Override
     public void start(Stage primaryStage) {
 
-        Bloom bloom = new Bloom();
+        bloom = new Bloom();
         bloom.setThreshold(0.1);
 
-        ImageView imageview = new ImageView(new Image(getClass().getResourceAsStream("/effect/images/JavaFX.png")));
+        imageview = new ImageView(new Image(getClass().getResourceAsStream("/effect/images/JavaFX.png")));
         imageview.setEffect(bloom);
+
+        GridPane gridpane = createPane();
 
         BorderPane borderpane = new BorderPane();
         borderpane.setCenter(imageview);
+        borderpane.setBottom(gridpane);
 
         // Set the Layout Pane of Scene
         Scene scene = new Scene(borderpane);
         // Set the title of Stage
         primaryStage.setTitle("Bloom Demo");
         // Set the width of Stage
-        primaryStage.setWidth(300);
+        primaryStage.setWidth(500);
         // Set the height of Stage
-        primaryStage.setHeight(300);
+        primaryStage.setHeight(400);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
 
         // Show Stage
         primaryStage.show();
+    }
+
+    private GridPane createPane() {
+
+        // Set Threshold
+        Label title1 = new Label("Threshold:");
+        title1.setPrefWidth(70);
+
+        final Label lblValue1 = new Label(new DecimalFormat("0.0").format(bloom.getThreshold()));
+        lblValue1.setPrefSize(100, 25);
+
+        final Slider slider1 = new Slider();
+        slider1.setOrientation(Orientation.HORIZONTAL);
+        slider1.setMin(0.0);
+        slider1.setMax(1.0);
+        slider1.setValue(bloom.getThreshold());
+        slider1.setMajorTickUnit(0.1);
+        slider1.setBlockIncrement(0.1);
+        slider1.setPrefSize(200, 25);
+        slider1.setSnapToTicks(true);
+
+        slider1.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            bloom.setThreshold(slider1.getValue());
+            lblValue1.setText(new DecimalFormat("0.0").format(slider1.getValue()));
+        });
+
+        GridPane gridpane = new GridPane();
+        gridpane.setStyle("-fx-background-color: #d4d0c8;");
+        gridpane.setPadding(new Insets(10, 10, 10, 10));
+        gridpane.setHgap(5);
+        gridpane.setVgap(5);
+        gridpane.add(title1, 0, 0);
+        gridpane.add(slider1, 1, 0);
+        gridpane.add(lblValue1, 2, 0);
+
+        return gridpane;
     }
 
     /**
